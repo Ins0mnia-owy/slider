c202a3b9b29750caba2c18d53a6fad3f75eae197
Ins0mnia-owy <oshiwithyou@gmail.com>
2026-03-30 10:16:26 +0300

    fin

---
+++ js/index.js
@@ -0,0 +1,9 @@
+new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 50,
+    navigation: {
+        prevEl: '.shots__btn-prev',
+        nextEl: '.shots__btn-next',
+    }
+});
